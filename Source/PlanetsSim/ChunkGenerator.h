c435e9af14c8d6915340892ee9d8324c518b4ac2
UO276213 <uo276213@uniovi.es>
2022-07-09 18:21:28 +0200

    Base para desarrollar en C++

---
+++ Source/PlanetsSim/ChunkGenerator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ChunkGenerator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLANETSSIM_API AChunkGenerator : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
--- Source/PlanetsSim/PlanetsSimGameModeBase.cpp
+++ Source/PlanetsSim/PlanetsSimGameModeBase.cpp
@@ -1,5 +1,20 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "PlanetsSimGameModeBase.h"
+
+APlanetsSimGameModeBase::APlanetsSimGameModeBase() : Super()
+{
+	// Asignamos como DefaultPawn nuestro pesonaje
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/MyCharacterBP"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	static ConstructorHelpers::FClassFinder<APlayerController> ControllerClassAsset(
+		TEXT("/Game/Blueprints/ChunkRuntimeGeneratorBP"));
+	if (ControllerClassAsset.Succeeded())
+	{
+		PlayerControllerClass = ControllerClassAsset.Class;
+	}
+}
+
 
