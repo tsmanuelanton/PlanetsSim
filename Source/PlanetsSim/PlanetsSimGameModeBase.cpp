9ae70fedf43b8de0c4c5ba582616cbbe0dd6291b
UO276213 <uo276213@uniovi.es>
2022-07-29 01:33:34 +0200

    quitado aviso bp no encontrado

--- Source/PlanetsSim/PlanetsSimGameModeBase.cpp
+++ Source/PlanetsSim/PlanetsSimGameModeBase.cpp
@@ -5,16 +5,6 @@
 
 APlanetsSimGameModeBase::APlanetsSimGameModeBase() : Super()
 {
-	// Asignamos como DefaultPawn nuestro pesonaje
-	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/BP_MyCharacter"));
-	// DefaultPawnClass = PlayerPawnClassFinder.Class;
-
-	static ConstructorHelpers::FClassFinder<APlayerController> ControllerClassAsset(
-		TEXT("/Game/Blueprints/OLD_BP_PlayerController"));
-	if (ControllerClassAsset.Succeeded())
-	{
-		PlayerControllerClass = ControllerClassAsset.Class;
-	}
 }
 
 
